c413be157370cf3e7be60d15645940b73a4cf066
shettyvihan55-del <shettyvihan55@gmail.com>
2026-09-03 12:45:29 +0530

    Add files via upload

---
+++ compop.py
@@ -0,0 +1,27 @@
+#comparision operators
+a = 10
+b = 20
+print(a ==b)
+print(a !=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+
+#age eligiblity checker
+age = int (input("enter your age:"))
+print("ELIGBILE",age>= 18)
+
+#result
+marks = int(input("enter the number:"))
+print("result=", marks>=40 )
+
+#login validation
+correct_username = "admin"
+correct_password = 1234
+old_username = input("enter the user name:")
+old_password = int(input("enter the password:"))
+print(old_username ==correct_username)
+print(old_password ==correct_password)
+
+
