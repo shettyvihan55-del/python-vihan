96ffb667bbc592601117b0320d7595d5550aa8d5
shettyvihan55-del <shettyvihan55@gmail.com>
2026-09-19 12:23:40 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,165 @@
+# print("hello class")
+# age=int(input("enter your age:"))
+# if(age >=18):
+#     print("eligible for vote")
+# else:
+#     print("not eligible for vote")
+# print("test completed")   
+
+# n = int(input("enter a number:"))
+# if(n == 1):
+#     print("MRDU")
+# elif(n == 2):
+#     print("svu")
+# elif(n == 3):
+#     print("mbu")
+# else:
+#     print("select any another college")
+# num = int(input("enter a number:"))
+# if(num >0):
+#     if(num<30):
+#         print("number is in range of 1 to 30")
+#     else:
+#         print("number is greater than 30")
+# else:
+#     print("number is -ve one")
+# number = int(input("enter your number"))
+# if(number>0):
+#     print("postive")
+# else:
+#     print("negative number ")
+# mark = int(input("enter your marks:"))
+# if(mark>=35):
+#     print("pass")
+# else:
+#     print("fail")
+# a = int(input("enter the number:"))
+# if(a %5==0) :
+#     print("divisible by 5")
+# else:
+#     print("not divisible by 5")
+
+# b= float(input("enter the temperature:"))
+# if(b>=40):
+#     print("temparature is high")
+# else:
+#      print("temparature is normal")
+# a = int(input("enter the number:"))
+# b = int(input("entert the another number:"))
+# if(a>b):
+#     print("a is greater")
+# else:
+#     print("b is greater")
+# year = int(input("enter the year :"))
+# if (year%4==0):
+#     print("leap year")
+# else:
+#     print("non-leap year")
+# a = int(input("enter the number:"))
+# if(a>=100):
+#     print("number is greater than equals to 100")
+# else:
+#     print("number is less than 100")
+# aman = input("enter the charachter:")
+# if (aman in "aeiou"):
+#     print("vowels")
+# else:
+#     print("variables")
+
+# marks = int(input("enter your marks:"))
+# if marks>=90 :
+#     print("A grade")
+# elif marks>=75:
+#     print("B grade")
+# elif marks>=50 :
+#     print("C grade")
+# elif marks >=40 :
+#     print("D grade")
+# else:
+#     print("fail")
+# a = int(input("enter a number:"))
+# b = int(input("enter second number:"))
+# c = int(input("enter the third number:"))
+
+# if a>=b and a>=c :
+#     print("a is largest")
+# elif b>=a and b>=c :
+#     print("b is largest")
+# else :
+#     print("c is largest")
+# a = int(input("enter the number:"))
+# b = int(input("enter another number:"))
+# operator = input("ENTER operator(+,-,*,/):")
+# if operator == "+" :
+#     print("result :",a+b)
+# elif operator == "-" :
+#     print("result:",a - b)
+# elif operator == "*" :
+#     print("result:",a*b)
+# elif operator == "/" :
+#     if b!=0 :
+#         print("result:",
+#         a/b)
+#     else:
+#         print("not divisible")
+# else:
+#     print("no operator")
+# name = input("enter you name:")
+# password = input("enter the password")
+
+# if name=="vihan":
+#     if password=="1234":
+#         print("login succesfull")
+#     else:
+#         print("wrong  password")
+# else:
+#     print("login invalid")
+
+# balance = float(input("enter the balance:"))
+# withdraw = float(input("enter the withdraw"))
+# balance-=withdraw
+# if withdraw>0:
+#     if withdraw<=balance:
+#         print("withdraw succefull",balance)
+#     else:
+#         print("not sufficient balance")
+# else:
+#     print("invalid withdraw")
+
+# marks = int(input("enter the marks:"))
+# attendence = int(input("enter the percentage:"))
+# if marks>75:
+#     if attendence>75:
+#         print("selected for the college")
+#     else:
+#         print("low attendence")
+# else:
+#     print("low marks")
+# age =int(input("enter the age:"))
+# test = input("enter the result:")
+# if age>=18:
+#     if test=="pass":
+#         print("driving session pass")
+#     else:
+#         print("driving test fail")
+# else:
+#     print("you are a minor")
+
+
+#BODMAS
+"""
+RESULT=23*(2+3)-33
+print(RESULT)
+"""
+#for loop
+#Used to repeat code or iterate through a sequence
+for i in range(1,6):
+    print(i)
+
+
+
+
+
+
+
+
